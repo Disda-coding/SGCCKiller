63382259be178d2f693f42144015630f41db37e4
Disda-coding <497457669@qq.com>
2021-12-05 22:21:40 +0800

    version 1.1

---
+++ src/main/java/utils/ConfigFilter.java
@@ -0,0 +1,20 @@
+package utils;/**
+ * @Author disda
+ * @Date 2021/12/5
+ */
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * @program: QnA
+ * @description: 用于过滤配置文件
+ * @author: liu yan
+ * @create: 2021-12-05 18:36
+ */
+public class ConfigFilter implements FilenameFilter {
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.endsWith(".yml")||name.endsWith(".yaml");
+    }
+}
